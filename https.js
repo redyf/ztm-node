104ccf6d2c9de33ce7880209e875b68f1d10db17
Redyf <mateusalvespereira7@gmail.com>
2024-05-04 20:02:26 -0300

    feat: add initial commit

---
+++ https.js
@@ -0,0 +1,11 @@
+const { send, read } = require("./internals");
+// const { send } = require("./request");
+// const { read } = require("./response");
+
+function makeRequest(url, data) {
+  send(url, data);
+  return read();
+}
+
+const responseData = makeRequest("https://google.com", "hello");
+console.log(responseData);
---
+++ internals/response.js
@@ -0,0 +1,10 @@
+function decrypt(data) {
+  return "decrypt data";
+}
+function read() {
+  return decrypt("data");
+}
+
+module.exports = {
+  read,
+};
